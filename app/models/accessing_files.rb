cbb7a7f57a739bb919618fed65788d02a3ef1bff
Thomas Scherz <thomas.scherz@uc.edu>
2013-10-25 00:09:04 -0400

    imported urls into array

---
+++ app/models/accessing_files.rb
@@ -0,0 +1,24 @@
+file = File.new('bookmark_url.lst', 'r')
+file.close
+
+File.open('bookmark_url.lst', 'r') do |file|  
+contentsArray = [] 
+while line = file.gets
+#     puts "** " + line.chomp + " **"
+     contentsArray.push line
+end
+
+#f = File.open('bookmark_url.lst', 'r') do |file| 
+#File.each_line (|line| puts line)
+#   contentsArray = []
+#   f.each_line {|line|
+#      contentArray.push line
+#   }
+
+puts contentsArray[0]
+
+end
+
+
+
+
--- app/models/sed_script.sh
+++ app/models/sed_script.sh
@@ -1,1 +1,13 @@
 sed -ne 's|.*\<A HREF="\(http[^"]*\)".*|\1|p' bookmarks.html > bookmark_url.lst 
+
+irb --single-prompt
+file = File.new('bookmark_url.lst', 'r')
+file.gets
+file.gets.chomp
+
+file.read(4)
+file.puts
+file.close
+
+ruby accessing_files.rb
+
